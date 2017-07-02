9af21bc81770998e1627a58928c12fd3b77cbfdd
Gianluca Corrado <corrag@gmail.com>
2017-07-02 13:22:29 +0100

    Update __init__.py

--- protscan/__init__.py
+++ protscan/__init__.py
@@ -1,7 +1,7 @@
 """init."""
 
 __author__ = "Gianluca Corrado"
-__copyright__ = "Copyright 2016, Gianluca Corrado"
+__copyright__ = "Copyright 2016-2017, Gianluca Corrado"
 __license__ = "MIT"
 __maintainer__ = "Gianluca Corrado"
 __email__ = "gianluca.corrado@unitn.it"
